bdace136beb287cb2c5266b016a6325d204f0702
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-09-27 11:17:55 +0600

    Complete SignIn Section

--- lib/ui/home/sign_in_screen.dart
+++ lib/ui/home/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_managment_apk/ui/widget/app_color.dart';
 import 'package:task_managment_apk/ui/widget/screen_background.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -15,47 +16,100 @@ class _SignInScreenState extends State<SignInScreen> {
 
     return Scaffold(
       body: ScreenBackground(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 110,
-              ),
-              Text(
-                'Get Started With',
-                style: textTheme.displaySmall!
-                    .copyWith(fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 110,
                 ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
+                Text(
+                  'Get Started With',
+                  style: textTheme.displaySmall!
+                      .copyWith(fontWeight: FontWeight.w500),
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  child:  const Icon(Icons.arrow_circle_right_outlined))
-            ],
+                const SizedBox(
+                  height: 24,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                ),
+                _buildSignInFormSection(),
+                const SizedBox(
+                  height: 24,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: _onTapForgotPasswordButton,
+                        child: const Text('Forgot Password?',style: TextStyle(
+                          color: Colors.grey
+                        ),),
+                      ),
+                      _buildSignUpSection(),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  void _onTapForgotPasswordButton(){
+    //TODO: implement forgot password
+  }
+
+  Widget _buildSignUpSection() {
+    return RichText(
+      text: const TextSpan(
+        style: TextStyle(
+            color: Colors.black, fontSize: 14, fontWeight: FontWeight.w600),
+        text: "Don't have an account? ",
+        children: [
+          TextSpan(
+            text: 'Sign Up',
+            style: TextStyle(color: AppColor.themeColor),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignInFormSection() {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: const InputDecoration(
+            hintText: 'Password',
+          ),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        ElevatedButton(
+          onPressed: _onTapNextScreenButton,
+          child: const Icon(Icons.arrow_circle_right_outlined),
+        ),
+      ],
+    );
+  }
+
+  void _onTapNextScreenButton() {
+    //TODO: implement next Screen Button
+  }
+
+
 }
