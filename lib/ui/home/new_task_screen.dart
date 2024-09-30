61ca504a0771454f19f6d9dce099cc350470786a
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-10-01 00:27:16 +0600

    new task screen complete

--- lib/ui/home/new_task_screen.dart
+++ lib/ui/home/new_task_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:task_managment_apk/ui/home/add_new_task_bar.dart';
 
-class NewTaskScreen extends StatelessWidget {
+class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
 
   @override
+  State<NewTaskScreen> createState() => _NewTaskScreenState();
+}
+
+class _NewTaskScreenState extends State<NewTaskScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('New'),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTapFAB,
+        child: const Icon(Icons.add),
+      ),
     );
+  }
+
+  void _onTapFAB() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const AddNewTaskBar()));
   }
 }
