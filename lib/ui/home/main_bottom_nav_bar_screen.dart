8a0b28b5d5230ce94caca1b96f1ffe9c65eae188
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-09-30 22:48:49 +0600

    Complete Navigation BAr

---
+++ lib/ui/home/main_bottom_nav_bar_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MainBottomNavBarScreen extends StatefulWidget {
+  const MainBottomNavBarScreen({super.key});
+
+  @override
+  State<MainBottomNavBarScreen> createState() => _MainBottomNavBarScreenState();
+}
+
+class _MainBottomNavBarScreenState extends State<MainBottomNavBarScreen> {
+
+  int _selectedIndex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index){
+          _selectedIndex = index;
+          setState(() {});
+        },
+        destinations: const[
+           NavigationDestination(icon: Icon(Icons.new_label), label: 'New'),
+           NavigationDestination(icon: Icon(Icons.check_box), label: 'Completed'),
+           NavigationDestination(icon: Icon(Icons.cancel_presentation_rounded), label: 'Canceled'),
+           NavigationDestination(icon: Icon(Icons.incomplete_circle), label: 'Progress'),
+        ],
+      ),
+    );
+  }
+}
