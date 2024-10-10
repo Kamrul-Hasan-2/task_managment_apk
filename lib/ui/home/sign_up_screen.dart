d593236c9e7d6fc307ba39a684c2c8b51b3d3f74
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-10-11 00:21:51 +0600

    add controller

--- lib/ui/home/sign_up_screen.dart
+++ lib/ui/home/sign_up_screen.dart
@@ -11,6 +11,13 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
@@ -34,7 +41,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 const SizedBox(
                   height: 24,
                 ),
-
                 _buildSignUpFormSection(),
                 const SizedBox(
                   height: 24,
@@ -54,11 +60,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
     );
   }
 
-
-
   Widget _buildSignUpSection() {
     return RichText(
-      text:  TextSpan(
+      text: TextSpan(
         style: const TextStyle(
             color: Colors.black, fontSize: 14, fontWeight: FontWeight.w600),
         text: "Have account? ",
@@ -66,77 +70,132 @@ class _SignUpScreenState extends State<SignUpScreen> {
           TextSpan(
               text: 'Sign In',
               style: const TextStyle(color: AppColor.themeColor),
-              recognizer: TapGestureRecognizer()..onTap = _onTapSignInButton
-          ),
+              recognizer: TapGestureRecognizer()..onTap = _onTapSignInButton),
         ],
       ),
     );
   }
 
   Widget _buildSignUpFormSection() {
-    return Column(
-      children: [
-        TextFormField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(
-            hintText: 'Email',
-          ),
-        ),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _emailTEController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Email',
+            ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Email is required';
+              }
+              if (!value!.contains('@')) {
+                return 'Enter a valid Email "@"';
+              }
+              if (!value.contains('.com')) {
+                return 'Enter a valid Email ".com"';
+              }
+              return null;
+            },
 
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'First Name',
           ),
-        ),
-
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'Last Name',
+          const SizedBox(
+            height: 8,
           ),
-        ),
-
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          keyboardType: TextInputType.phone,
-          decoration: const InputDecoration(
-            hintText: 'Mobile',
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _firstNameTEController,
+            decoration: const InputDecoration(
+              hintText: 'First Name',
+            ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'First Name is required';
+              }
+              return null;
+            },
           ),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'Password',
+          const SizedBox(
+            height: 8,
           ),
-        ),
-        const SizedBox(
-          height: 24,
-        ),
-        ElevatedButton(
-          onPressed: _onTapNextScreenButton,
-          child: const Icon(Icons.arrow_circle_right_outlined),
-        ),
-      ],
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _lastNameTEController,
+            decoration: const InputDecoration(
+              hintText: 'Last Name',
+            ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Last Name is required';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _mobileTEController,
+            keyboardType: TextInputType.phone,
+            decoration: const InputDecoration(
+              hintText: 'Mobile',
+            ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Mobile is required';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _passwordTEController,
+            decoration: const InputDecoration(
+              hintText: 'Password',
+            ),
+            validator: (String? value) {
+              if (value?.isEmpty ?? true) {
+                return 'Password is required';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          ElevatedButton(
+            onPressed: _onTapNextScreenButton,
+            child: const Icon(Icons.arrow_circle_right_outlined),
+          ),
+        ],
+      ),
     );
   }
 
   void _onTapNextScreenButton() {
-    //TODO: implement next Screen Button
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
   }
 
-  void _onTapSignInButton(){
-      Navigator.pop(context);
+  void _onTapSignInButton() {
+    Navigator.pop(context);
   }
 
-
-
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _mobileTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
+  }
 }
