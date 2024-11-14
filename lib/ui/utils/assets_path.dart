7a593f24b3cac546aa3b4f4fc40a1c17d3ff62d3
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-11-15 00:24:58 +0600

    Delete

--- lib/ui/utils/assets_path.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AssetsPath {
-  static const String _imagesPath = 'assets/images/';
-
-  static const backGroundSvg = '${_imagesPath}background.svg';
-  static const logoSvg = '${_imagesPath}logo.svg';
-}
--- lib/ui/widget/snack_bar_message.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void snackBarMessage(BuildContext context, String message,
-    [bool isError = false]) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
-    backgroundColor: isError ? Colors.red : Colors.green,
-  ));
-}
