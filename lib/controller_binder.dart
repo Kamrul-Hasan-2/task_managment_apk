6a79d6011aade2274ad16546234223753c4f3dbf
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-11-16 00:39:14 +0600

    Completed List Screen Convert in GetX

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,9 +1,15 @@
 import 'package:get/get.dart';
+import 'package:task_managment_apk/ui/controller/completed_task_controller.dart';
+import 'package:task_managment_apk/ui/controller/new_task_list_controller.dart';
 import 'package:task_managment_apk/ui/controller/sign_in_controller.dart';
+import 'package:task_managment_apk/ui/controller/task_status_list_controller.dart';
 
 class ControllerBinder extends Bindings{
   @override
   void dependencies() {
     Get.put(SignInController());
+    Get.put(NewTaskListController());
+    Get.put(TaskStatusListController());
+    Get.put(CompletedTaskController());
   }
 }
