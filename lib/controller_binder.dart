cc915f6db302058ed8d36833f371091890d57201
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-11-16 01:14:59 +0600

    Progress List Screen Convert in GetX

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:task_managment_apk/ui/controller/canceled_task_controller.dart';
 import 'package:task_managment_apk/ui/controller/completed_task_controller.dart';
 import 'package:task_managment_apk/ui/controller/new_task_list_controller.dart';
+import 'package:task_managment_apk/ui/controller/progress_task_controller.dart';
 import 'package:task_managment_apk/ui/controller/sign_in_controller.dart';
 import 'package:task_managment_apk/ui/controller/task_status_list_controller.dart';
 
@@ -13,5 +14,6 @@ class ControllerBinder extends Bindings{
     Get.put(TaskStatusListController());
     Get.put(CompletedTaskController());
     Get.put(CanceledTaskController());
+    Get.put(ProgressTaskController());
   }
 }
